396d0d803242fc3f64ccecadb733f6dda4530cfe
JONATHAN FELIPE VELASQUEZ PEREZ <ia.then@ufg.edu.sv>
2019-11-19 12:34:58 -0600

    Plantilla

---
+++ 3NCRIPTACION.asm
@@ -0,0 +1,346 @@
+include 'emu8086.inc'
+
+org 100h
+
+;/////////////////////////////////////////////////////////////////
+MPRINTTEXT macro TextToPrint
+    mov ah, 9 
+    mov dx, offset TextToPrint 
+    int 21h 
+ENDM
+;/////////////////////////////////////////////////////////////////
+
+;/////////////////////////////////////////////////////////////////
+;///////////////READING TEXT UNIVERSAL////////////////////////////
+;/////////////////////////////////////////////////////////////////
+MREADINGTEXT MACRO TextToFill
+    
+    ReadCharacter:
+        ;Preparamos el poder de la lectura
+        mov ah, 1
+        int 21h
+        
+        ;Sera un Enter?
+        cmp al, 13
+        jz FinishReader
+        
+        ;Quiere borrar Texto?
+        ;cmp al, 8
+        cmp al, 8
+        jz BackDelete
+        
+        ;Continuar normal
+        sub al, 32
+        mov TextToFill[si], al
+        inc si
+        jmp ReadCharacter
+    BackDelete:
+        dec si
+        mov TextToFill[si], '$'
+        jmp ReadCharacter  
+        
+    FinishReader:
+    ;This is only to finish the macro
+       
+    
+ENDM
+;/////////////////////////////////////////////////////////////////
+
+
+
+;/////////////////////////////////////////////////////////////////
+;/////////////START THE PROGRAM///////////////////////////////////
+;/////////////////////////////////////////////////////////////////
+
+StartProgram:
+ 
+CALL PPRINTHEADER       
+MAINMENU: 
+  
+mov ah, 1                              ;0=48 / 1=49 / 2=50 Encriptar cadena de texto / 3=51 /   
+int 21h 
+;LEYENDO TECLADO
+;cmp al, 48
+;jz encriptar archivo
+;cmp al, 49
+;jz desencriptar un archivo 
+cmp al, 49
+jz VOID_ENCRIPTAR
+
+cmp al, 50 
+jz VOID_ENCRIPTAR
+ 
+
+jmp error5
+;cmp al, 51
+;jz salir del programa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;/////////////////////////////////////////////////////////////////
+;///////////////////FINISH THE START PROGRAM//////////////////////
+;/////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+;/////////////////////////////////////////////////////////////////
+;/////////////END THE PROGRAM/////////////////////////////////////
+;/////////////////////////////////////////////////////////////////
+EndProgram:
+RET
+;/////////////////////////////////////////////////////////////////
+;/////////////END THE PROGRAM/////////////////////////////////////
+;/////////////////////////////////////////////////////////////////
+
+
+;/////////////////////////////////////////////////////////////////
+PPRINTHEADER PROC
+    MOV AH,00H ;FUNCION PARA LIMPIAR PANTALLA
+    MOV AL,03H ;MODO TEXTO 80X25
+    INT 10H
+    
+    MPRINTTEXT varline1
+    MPRINTTEXT salto
+    MPRINTTEXT titulo
+    MPRINTTEXT salto
+    MPRINTTEXT varline1
+    
+    MPRINTTEXT salto
+    MPRINTTEXT varline1
+    MPRINTTEXT salto
+    MPRINTTEXT msgdescrip
+    MPRINTTEXT salto
+    MPRINTTEXT msgver
+    MPRINTTEXT salto 
+    
+    MPRINTTEXT varline2
+    MPRINTTEXT msg1
+    MPRINTTEXT msg2
+    MPRINTTEXT msg3
+    MPRINTTEXT msg4
+    MPRINTTEXT msg5
+    MPRINTTEXT salto
+    jmp MAINMENU
+        
+PPRINTHEADER ENDP
+;/////////////////////////////////////////////////////////////////
+
+;/////////////////////////////////////////////////////////////////
+;/////////////////ENCRIPT METHOD//////////////////////////////////
+;/////////////////////////////////////////////////////////////////
+VOID_ENCRIPTAR:
+    MOV contadorllave,0
+    MOV llave, 0
+    MPRINTTEXT salto
+    MPRINTTEXT msgllave
+    MPRINTTEXT salto
+    
+    ReadCharacter:
+        ;Preparamos el poder de la lectura
+        mov ah, 1
+        int 21h
+        
+        ;Sera un Enter?
+        cmp al, 13
+        jz FinishReader
+        
+        ;Quiere borrar Texto?
+        ;cmp al, 8
+        cmp al, 8
+        jz BackDelete
+        
+        ;Continuar normal
+        sub al, 32
+        mov llave[si], al
+        inc si
+        inc contadorllave ; Tambien le sumamos al contador
+        jmp ReadCharacter
+    BackDelete:
+        dec si
+        mov llave[si], '$'
+        jmp ReadCharacter  
+    
+    FinishReader:
+        
+;---END TO READ, NOW LETS OPEN THE FILE TO ENCRIPT
+    
+crear: 
+    mov ah, 3Ch 
+    mov cx, 0 
+    mov dx, offset fileout 
+    int 21h 
+    jc error3 
+    mov handler2, ax     
+    
+OpenFile: 
+    mov ah, 3dh 
+    mov al, 2 
+    mov dx, offset filein 
+    int 21h 
+    ;jc error1 
+    mov handler, ax  
+      
+ReadFile: 
+    mov bx, handler 
+    mov ah, 3fh 
+    mov cx, 10 
+    mov dx, offset fragmento 
+    int 21h   
+    ;jc error2          
+    cmp ax, 0 ;? EOF 
+    jz CloseFile              
+
+WriteTheEncryption: 
+    call EncryptProcedure 
+    
+    mov ah, 40h 
+    mov bx, handler2 
+    mov cx, 10  ; Number of bytes
+    mov dx, offset fragmento 
+    int 21h 
+    jc error4 
+    jmp MAINMENU
+    
+Clean: 
+    ;;limpiar varible 
+    mov si, offset limpio 
+    mov di, offset fragmento 
+    mov cx, 10 
+    rep movsb     
+         
+    
+CloseFile: 
+    ;cerrar archivo 
+    mov ah, 3eh 
+    mov bx, handler 
+    int 21h 
+     
+    mov bx, handler2 
+    int 21h
+         
+
+;---WELL, NOW GO TO MAINMENU AGAIN
+jmp MAINMENU
+ 
+;/////////////////////////////////////////////////////////////////
+;////////////////////END OF ENCRIPT METHOD////////////////////////
+;/////////////////////////////////////////////////////////////////
+
+EncryptProcedure PROC 
+    ;Make the count
+    
+     
+    ;Sumando 2 a su codigo ASCI 
+    xor si,si  
+    mov cx,10 
+    mov dx, offset fragmento 
+    hacer: 
+        mov al, fragmento[si] 
+        cmp al,000        
+        jnle siguiente 
+        cmp al,255 
+        jnge siguiente 
+       
+        siguiente: 
+            inc al
+            inc al
+            mov fragmento[si],al
+             
+            inc si 
+    loop hacer 
+    ret
+          
+EncryptProcedure ENDP  
+
+;/////////////////////////////////////////////////////////////////                                                                  
+;/////////////////////ERROR///////////////////////////////////////    
+;/////////////////////////////////////////////////////////////////
+error1: 
+    MPRINTTEXT msgError1 
+    jmp fin 
+     
+error2: 
+    MPRINTTEXT msgError2 
+    jmp fin 
+     
+error3: 
+    MPRINTTEXT msgError3 
+    jmp fin  
+ 
+error4: 
+    MPRINTTEXT msgError4 
+    jmp fin 
+fin: 
+
+error5:
+    MPRINTTEXT ErrorN1
+    CALL PPRINTHEADER
+    jmp MAINMENU
+
+ret                                                                      
+;/////////////////////////////////////////////////////////////////                                                                  
+;/////////////////////STUFF///////////////////////////////////////    
+;/////////////////////////////////////////////////////////////////
+ 
+        
+msgError1 db 10,13,'Error: No se pudo abrir el archivo. $' 
+msgError2 db 10,13,'Error: Al leer los caracteres$' 
+msgError3 db 10,13,'Error: No se puede crear el archivo. $' 
+msgError4 db 10,13,'Error: No se puede escribir el archivo. $'
+
+  
+;variables de usos varios 
+
+varline1 dw 254,254,254,254,254,254,254,254,254,254,254,254, '$'
+varline2 dw 177,177,177,177,177,177,177,177,177,177,177,177, '$'
+salto db 10,13,'$'
+
+;variables de titulo
+msgver db '(Version < 1) $'
+msgdescrip db 'Encriptador y desencriptador de texto $'
+titulo db '        LeftKey$' 
+
+;variables de entrada de texto
+
+msg1 db 10,13,'Elegir opcion a ejecutar: $'       
+msg2 db 10,13,'Presione la tecla 0 para encriptar un archivo. $'
+msg3 db 10,13,'Presione la tecla 1 para desencriptar un archivo. $'
+msg4 db 10,13,'Presione la tecla 2 para encriptar una cadena de texto. $'
+msg5 db 10,13,'Presione la tecla 3 para salir del programa. $'           
+msgllave db 10,13,'Escriba una palabra que se utilizara como llave para encriptar su texto: $'
+
+ErrorN1 db 10,13,'Esa opcion no es posible :( $'
+
+;variable para guardado de texto
+llave db 20 dup('$')
+contadorllave db 0
+frase db 20 dup('$')
+bytes db 0
+
+handler dw ?  
+handler2 dw ?
+filein db 'C:\prueba\ToEncript.txt',0
+fileout db 'C:\prueba\EncriptedFile.txt',0 
+fragmento db '$$$$$$$$$$$$$$$$$$$$$$$',0 
+limpio db '                     ',0 
